bd03599bb16c582ffad5e4fe51cf83f4443ad0dd
sina_naghdali <naghdali.sina@gmail.com>
2023-11-15 15:08:45 +0330

    created render html file
    addad templates folder and html files to website app and render to view

--- mysite/views.py
+++ mysite/views.py
@@ -4,10 +4,10 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index_view(reguest):
-    return HttpResponse('<h1> home page </h1>')
+    return render(reguest,'website/index.html')
 
 def about_view(reguest):
-    return HttpResponse('<h1> about page </h1>')
+    return render(reguest,'website/about.html')
 
 def contact_view(reguest):
-    return HttpResponse('<h1> contct page </h1>')
+    return render(reguest,'website/contact.html')
